53c0a4c98fbe369953d5014e463e8c3de910769a
miasa <mia.santanen@gmail.com>
2015-02-13 18:36:11 +0200

    Cleaned up files, added comments, reordered folder structure

--- js/popup.js
+++ js/popup.js
@@ -1,25 +1,21 @@
 function highlight(wordLengths) {
-
-  console.log('popup.js find words with length: ' + wordLengths);
-
+  //Send word character counts to content script
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.sendMessage(tabs[0].id, {highlight: wordLengths}, function(response) {
       console.log(response);
     });
   });
-
-  //chrome.tabs.executeScript(null, {file: "content_script.js"});
 }
 
+//Form elements
 var form = document.getElementById("relaxtension-popup"),
     input = document.getElementById("word-count");
 
+//Set focus on popup init
 input.focus();
 
+//Listen for submit event
 form.addEventListener("submit", function(e) {
   e.preventDefault();
-
-  var inputVal = input.value;
-  highlight(inputVal);
-
+  highlight(input.value);
 }, false);
