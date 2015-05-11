fa1ce40a8c0a723b76408b9637126413fa4709ec
miasa <mia.santanen@gmail.com>
2015-05-11 21:18:27 +0300

    Changed .highlight class to a more specific .relaxtension-highlight, to avoid unwanted styles in case the website uses the same class names

--- js/content_script.js
+++ js/content_script.js
@@ -1,6 +1,14 @@
 (function($) {
-  function checkLength(wordLengths) {
-    //TODO
+
+  function prepareText(textToSearch) {
+    //Sanitize text
+
+    //Combine multiple line feeds into one
+    textToSearch = textToSearch.replace(/[\n\t]{2,}/g, "\n");
+    //Replace line feed with space
+    textToSearch = textToSearch.replace(/[\n]/g, " ");
+
+    return textToSearch;
   }
 
   chrome.runtime.onMessage.addListener(
@@ -8,67 +16,68 @@
       if (typeof request.highlight !== undefined) {
 
           var body = $('body'),
-              textToSearch = body.text(),
-              lengthInput = request.highlight,
+              textToSearch = prepareText(body.text()),
+              wordLengths = request.highlight.split(" "),
               matches = [];
 
-          //Sanitize text
-
-          //Remove multiple line feeds
-          textToSearch = textToSearch.replace(/[\n\t]{2,}/g, "\n");
-          //Remove sentence ends
-          textToSearch = textToSearch.replace(/[!?:;,.()"']/g, "");
-          //Split by space
-          var words = textToSearch.split(" ");
-
-          //Clear previous highlights
-          $("body").unhighlight();
-
-          if(lengthInput.split(" ").length > 1) {
-            //Multiple words
-            var wordLengths = lengthInput.split(" ");
-
-            //Check for number type
-            $.each(wordLengths, function(i, length) {
-              var lengthNumber = parseInt(length, 10);
-              if(!isNaN(lengthNumber)) {
-                wordLengths[i] = lengthNumber;
-              }
-            });
-
-            //Check each word for first length
-            if(wordLengths.length) {
-              $.each(words, function(i, word) {
-                if(word.length === wordLengths[0]) {
-                  //First word found, check for second
-                  //TODO: should be recursive
-                }
-              });
+          //Check for number type
+          $.each(wordLengths, function(i, length) {
+            var lengthNumber = parseInt(length, 10);
+            if(!isNaN(lengthNumber)) {
+              wordLengths[i] = lengthNumber;
             }
-
-
-          }else {
-            //Check for number type
-            lengthInput = parseInt(lengthInput, 10);
-            if(!isNaN(lengthInput)) {
-
-              //Check each word for length
-              $.each(words, function(i, word) {
-                if(word.length === lengthInput) {
-                  matches.push(word);
-                }
-              });
-            }
-          }
-
-          //Highlight found words
-          $.each(matches, function(i, match) {
-              body.highlight(match);
           });
 
+          var patternWordPart = "[a-zäöåéèáàëêâ\-]", //Crude replacement for utf8 \w
+              patternWordBoundary = "[\\s.:,!?()\\\"']", //Crude replacement for utf8 word boundary \b
+              pattern = "";
+
+          $.each(wordLengths, function(i, length) {
+            pattern += (i === 0 ? patternWordBoundary : "\\s") + patternWordPart + "{" + length + "}";
+          });
+
+          pattern += patternWordBoundary;
+
+          //console.log('text', textToSearch);
+          console.log('pattern', pattern);
+
+          var regex = new RegExp(pattern,"gi"), 
+              matches = [], 
+              match;
+
+          while(match = regex.exec(textToSearch)) {
+              matches.push(match);
+              regex.lastIndex = match.index + 1; //For overlapping matches
+          }
+
+          //var matches = textToSearch.match(new RegExp(pattern, "gi"));
+
+          console.log('matches', matches);
+
+          //Clear previous highlights
+          body.unhighlight();
+
+          if(matches && matches.length) {
+            
+            //Sanitize even more
+            $.each(matches, function(i, match) {
+              matches[i] = $.trim(matches[i]);
+              matches[i] = matches[i].replace(new RegExp("[.:,!?()\\\"']", "gi"), "");
+            });
+
+            //Highlight found words
+            body.highlight(matches, {className: 'relaxtension-highlight'});
+          }else {
+            alert("No matches found");
+          }
+
+          console.log('matches after', matches);
+
+          //For debugging purposes
           sendResponse('highlighted the words: ' + JSON.stringify(matches));
           return true;
       }
     }
   );
+
 })(jQuery);
